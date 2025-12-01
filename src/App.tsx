8c08a55c7c57026068d3ef4d19d300df2d0e5b37
Gabriel Guerreiro <136819754+GabrielGuerreir0@users.noreply.github.com>
2025-12-01 16:48:24 -0300

    sections

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,28 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
-  return <h1>Kairos</h1>;
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
+    </>
+  );
 }
