b7f8479934391bf75ef81dd14a3757cc721211df
Gabriel Guerreiro <136819754+GabrielGuerreir0@users.noreply.github.com>
2025-12-02 14:43:03 -0300

    initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,54 @@
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+
 import "./styles/theme.css";
 import "./styles/global.css";
+import { ContDown } from "./components/ContDown";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
+import { PlayCircleIcon } from "lucide-react";
+import { Footer } from "./components/Footer";
 
 export function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <ContDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput type="text" id="task" labelText="Vou trabalhar em" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <Cycles />
+          </div>
+
+          <div className="formRow">
+            <DefaultButton icon={<PlayCircleIcon />} color="green" />
+          </div>
+        </form>
+      </Container>
+
+      <Container>
+        <Footer />
+      </Container>
     </>
   );
 }
---
+++ src/components/DefaultButton/index.tsx
@@ -0,0 +1,21 @@
+import type React from "react";
+import styles from "./styles.module.css";
+
+type DefaultButtonProps = {
+  icon: React.ReactNode;
+  color: "green" | "red";
+} & React.ComponentProps<"button">;
+
+export function DefaultButton({
+  icon,
+  color = "green",
+  ...rest
+}: DefaultButtonProps) {
+  return (
+    <>
+      <button className={`${styles.button} ${styles[color]}`} {...rest}>
+        {icon}
+      </button>
+    </>
+  );
+}
