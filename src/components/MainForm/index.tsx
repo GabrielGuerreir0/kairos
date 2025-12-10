66756d64e3a47752c60f96b08537d69dc8b713c6
Gabriel Guerreiro <136819754+GabrielGuerreir0@users.noreply.github.com>
2025-12-10 16:19:26 -0300

    useRef in input

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,13 +4,47 @@ import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 import styles from "./styles.module.css";
-import { useState } from "react";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState("");
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+    if (!taskName) {
+      alert("digite a task");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: new Date(),
+      completeDate: null,
+      interruptedDate: null,
+      duration: 1,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: "00:00",
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
   return (
     <form onSubmit={handleCreateNewTask} className={styles.form} action="">
@@ -19,8 +53,7 @@ export function MainForm() {
           type="text"
           id="task"
           labelText="Vou trabalhar em"
-          value={taskName}
-          onChange={(e) => setTaskName(e.target.value)}
+          ref={taskNameInput}
         />
       </div>
 
